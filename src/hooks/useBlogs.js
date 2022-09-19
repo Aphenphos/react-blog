55eaf66fae7c5955bb13ee5bb20d0881aa17bd0a
Aphenphos <MaxwillWinters@gmail.com>
2022-09-19 15:08:21 -0400

    fixed syntax errors

--- src/hooks/useBlogs.js
+++ src/hooks/useBlogs.js
@@ -1,16 +1,17 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { getBlogs } from '../services/getBlogs';
 
 export function useBlogs() {
   const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
     async function fetchBlogs() {
-      const b = await fetchBlogs();
+      const b = await getBlogs();
+      console.log(b);
       setBlogs(b);
     }
     fetchBlogs();
   }, []);
-
-  return { blogs };
+  return blogs;
 }
